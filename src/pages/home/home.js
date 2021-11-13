79375c9422a947657509f5943d0d98702a8c6369
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-13 20:04:25 -0300

    mudança de px para rem

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,35 +1,34 @@
-import { ToastContainer } from 'react-toastify';
-import { ButtonGreen } from '../../components/CustomButton/CustomButton';
+import { ToastContainer } from "react-toastify";
+import { ButtonGreen } from "../../components/CustomButton/CustomButton";
 import Header from "../../components/Header/Header";
 import ModalAddEdit from "../../components/modais/modal-add-editar.js/modal-add-editar";
-import ModalExcluir from '../../components/modais/modal-excluir/modal-excluir';
+import ModalExcluir from "../../components/modais/modal-excluir/modal-excluir";
 import Tabela from "../../components/tabela/tabela";
 import useContatos from "../../hooks/useContatos";
-import './style.css';
+import "./style.css";
 
 export default function Home() {
-    const { setModalAddEdit, modalAddEdit, modalExcluir } = useContatos()
+  const { setModalAddEdit, modalAddEdit, modalExcluir } = useContatos();
 
-    return (
-        <div className="home">
-            <Header />
-            <section className="section">
-                <div className="home-container">
-                    <ButtonGreen
-                        className="btn-adicionar-contato"
-                        sx={{ mb: "32px", width: "230px" }}
-                        variant="contained"
-                        onClick={() => setModalAddEdit(true)}
-                    >
-                        Adicionar
-                    </ButtonGreen>
-                    <Tabela />
-                    {modalAddEdit && <ModalAddEdit />}
-                    {modalExcluir && <ModalExcluir />}
-                    <ToastContainer />
-                </div>
-            </section>
-
+  return (
+    <div className="home">
+      <Header />
+      <section className="section">
+        <div className="home-container">
+          <ButtonGreen
+            className="btn-adicionar-contato"
+            sx={{ mb: "3.2rem", width: "23rem" }}
+            variant="contained"
+            onClick={() => setModalAddEdit(true)}
+          >
+            Adicionar
+          </ButtonGreen>
+          <Tabela />
+          {modalAddEdit && <ModalAddEdit />}
+          {modalExcluir && <ModalExcluir />}
+          <ToastContainer />
         </div>
-    )
+      </section>
+    </div>
+  );
 }

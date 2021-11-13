85a130832bf6c0347b3d8275ceb0397e60e175e4
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-13 20:02:14 -0300

    criação de componente de célula personalizado

---
+++ src/components/tabela/table-cell/index.js
@@ -0,0 +1,8 @@
+import { TableCell } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledCell = styled(TableCell)({
+  fontSize: "1.6rem",
+});
+
+export default StyledCell;

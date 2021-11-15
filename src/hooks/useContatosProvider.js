44b56ea4c21569ae264a55378eba9cbd79966eb4
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-14 21:23:26 -0300

    states controlados dos inputs de login e cadastro movidos para os componentes de login e cadastro respectivamente

--- src/hooks/useContatosProvider.js
+++ src/hooks/useContatosProvider.js
@@ -1,56 +1,39 @@
 import { useState } from "react";
-import { useLocalStorage } from 'react-use'
-
+import { useLocalStorage } from "react-use";
 
 export default function useContatosProvider() {
-    const [user, setUser, removeUser] = useLocalStorage('user', {})
-    const [token, setToken] = useState(user ? user.token : '');
+  const [user, setUser, removeUser] = useLocalStorage("user", {});
+  const [token, setToken] = useState(user ? user.token : "");
 
-    const [inputLogin, setInputLogin] = useState({
-        email: '',
-        senha: ''
-    });
+  const [contato, setContato] = useState({
+    nome: "",
+    email: "",
+    telefone: "",
+  });
 
-    const [inputCadastro, setInputCadastro] = useState({
-        nome: '',
-        email: '',
-        senha: ''
-    });
+  const [modalAddEdit, setModalAddEdit] = useState(false);
 
-    const [contato, setContato] = useState({
-        nome: '',
-        email: '',
-        telefone: ''
-    })
+  const [modalExcluir, setModalExcluir] = useState(false);
 
-    const [modalAddEdit, setModalAddEdit] = useState(false);
+  const [listaContatos, setListaContatos] = useState([]);
 
-    const [modalExcluir, setModalExcluir] = useState(false)
+  const [contatoSelecionado, setContatoSelecionado] = useState({});
 
-    const [listaContatos, setListaContatos] = useState([]);
-
-    const [contatoSelecionado, setContatoSelecionado] = useState({})
-
-
-    return {
-        token,
-        setToken,
-        inputLogin,
-        setInputLogin,
-        inputCadastro,
-        setInputCadastro,
-        user,
-        setUser,
-        removeUser,
-        modalAddEdit,
-        setModalAddEdit,
-        contato,
-        setContato,
-        listaContatos,
-        setListaContatos,
-        contatoSelecionado,
-        setContatoSelecionado,
-        modalExcluir,
-        setModalExcluir
-    };
-};
+  return {
+    token,
+    setToken,
+    user,
+    setUser,
+    removeUser,
+    modalAddEdit,
+    setModalAddEdit,
+    contato,
+    setContato,
+    listaContatos,
+    setListaContatos,
+    contatoSelecionado,
+    setContatoSelecionado,
+    modalExcluir,
+    setModalExcluir,
+  };
+}

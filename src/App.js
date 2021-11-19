3fd74221ff753a47420323af9f56e3dd1dfa4ef7
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-19 13:46:11 -0300

    inclusão do providar das requests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import RotasProtegidas from './components/RotasProtegidas/RotasProtegidas';
-import ContatosProvider from './contexts/contatosProvider';
-import Cadastro from './pages/cadastro/cadastro';
-import Home from './pages/home/home';
-import Login from './pages/login/login';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import RotasProtegidas from "./components/RotasProtegidas/RotasProtegidas";
+import ContatosProvider from "./contexts/contatosProvider";
+import RequestsProvider from "./contexts/requestsProvider";
+import Cadastro from "./pages/cadastro/cadastro";
+import Home from "./pages/home/home";
+import Login from "./pages/login/login";
 
 function App() {
-
   return (
     <div className="app">
       <ContatosProvider>
-        <Router>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/cadastro" component={Cadastro} />
-            <RotasProtegidas>
-              <Route path="/" exact component={Home} />
-            </RotasProtegidas>
-          </Switch>
-        </Router>
+        <RequestsProvider>
+          <Router>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/cadastro" component={Cadastro} />
+              <RotasProtegidas>
+                <Route path="/" exact component={Home} />
+              </RotasProtegidas>
+            </Switch>
+          </Router>
+        </RequestsProvider>
       </ContatosProvider>
     </div>
   );
